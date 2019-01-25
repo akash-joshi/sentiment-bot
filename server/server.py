788a9b94eab76d4eafb76f58f88beaecebc1a953
Akash <akashnainani9@gmail.com>
2019-01-25 14:49:53 +0530

    added model to api
    added required pre-processing and rnn model for prediction.

--- server/server.py
+++ server/server.py
@@ -1,13 +1,37 @@
 from flask import (Flask,request)
 from flask_cors import CORS
+import pandas as pd
+import numpy as np
+
+import librosa
+from keras.models import load_model
+
 app = Flask(__name__)
 CORS(app)
+
+def toMfcc(file):
+   X, sample_rate = librosa.load(file, res_type='kaiser_fast')
+   mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
+   return mfccs
+
+def prePro(x):
+    res= pd.DataFrame(data=x) 
+    res = res.stack().to_frame().T
+    res= np.expand_dims(res, axis=2)
+    return res
+	
+model = load_model('rnn_mfcc_model_ver_1.h5')
 
 @app.route("/voice-checker",methods=["POST"])
 def secr():
     print(request.form.get('fname'))
     print(request.files)
-    return("server data")
+	audio_file=request.files
+	x=toMfcc(audio_file)
+	res=prePro(x)
+	result=model.predict(res)
+	result=result.argmax(axis=1)
+    return(result)
 
 if __name__ == "__main__":
     app.run(port='8000')    
