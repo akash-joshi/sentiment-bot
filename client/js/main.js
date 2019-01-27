8d209dc2a3e3ada687f2a8b83f9ce2759e341024
Unknown <jakash1997@gmail.com>
2019-01-27 16:43:50 +0530

    Chal gaya bhosdiwala

--- client/js/main.js
+++ client/js/main.js
@@ -3,24 +3,64 @@ const stop = document.querySelector('#stopbutton')
 const running = document.querySelector('#running')
 const { MediaRecorder, Blob } = window
 
-let repeat
-let num=1
+let num=1;
+let flag=true
 // disable stop button while not recording
 
 stop.disabled = true
 
-const constraints = { audio: true }
-//document.querySelector('#ouraudio').setAttribute('controls','')
-const onSuccess = (stream) => {
-  const mediaRecorder = new MediaRecorder(stream)
-  // generate a new file every 5s
+// main block for doing the audio recording
 
-  const record_and_send = () => {
+if (navigator.mediaDevices.getUserMedia) {
+  console.log('getUserMedia supported.')
+
+  const constraints = { audio: true }
+  document.querySelector('#ouraudio').setAttribute('controls','')
+  const onSuccess = (stream) => {
+    const mediaRecorder = new MediaRecorder(stream)
+    console.log(mediaRecorder)
+    const record_and_send = (mediaRecorder) => {
      console.log(num)
-     const chunks = [];
-     mediaRecorder.ondataavailable = e => chunks.push(e.data);
-     mediaRecorder.onstop = () => {
+     setTimeout(()=> mediaRecorder.stop(), 3000);
+     if(mediaRecorder.state != 'recording')
+     mediaRecorder.start();
+    }
+
+    let chunks = [];
+
+    const startFunc = (mediaRecorder) => {
+      console.log(mediaRecorder)
+      repeat = setInterval(()=>{record_and_send(mediaRecorder)}, 3000);
+    }
+
+    const stopFunc = () => {
+      clearInterval(repeat)
+    }
+
+    record.onclick = () => {
+      //mediaRecorder.start(6000)
+      startFunc(mediaRecorder)
       console.log(mediaRecorder.state)
+      console.log('recorder started')
+      running.style.display = ""
+      stop.disabled = false
+      record.disabled = true
+      record.style.display = "none"
+    }
+
+    stop.onclick = () => {
+      stopFunc()
+      //mediaRecorder.stop()
+      console.log(mediaRecorder.state)
+      console.log('recorder stopped')
+      running.style.display = "none"
+      record.style.display = ""
+      stop.disabled = true
+      record.disabled = false
+    }
+
+    mediaRecorder.onstop = () => {
+      console.log('data available after MediaRecorder.stop() called.')
       console.log('recorder stopped')
       const blob = new Blob(chunks, { 'type' : 'audio/wav' })
       const fd = new FormData();
@@ -36,65 +76,12 @@ const onSuccess = (stream) => {
       }).done(function(data) {
            console.log(data);
       });
-     }
-     mediaRecorder.start();
-     setTimeout(()=> mediaRecorder.stop(), 3000);
     }
 
-    const startFunc = () => {
-      repeat = setInterval(record_and_send, 3000);
-    }
-
-    const stopFunc = () => {
-      clearInterval(repeat)
-    }
-
-  record.onclick = () => {
-    startFunc()
-    console.log(mediaRecorder.state)
-    console.log('recorder started')
-    running.style.display = ""
-    stop.disabled = false
-    record.disabled = true
-    record.style.display = "none"
-
+    mediaRecorder.ondataavailable = e => chunks.push(e.data);
   }
 
-  stop.onclick = () => {
-    stopFunc()
-    running.style.display = "none"
-    record.style.display = ""
-    stop.disabled = true
-    record.disabled = false
-  }
-
-  /*mediaRecorder.onstop = () => {
-    console.log('data available after MediaRecorder.stop() called.')
-    console.log('recorder stopped')
-  }
-
-  mediaRecorder.ondataavailable = (e) => {
-    const blob = new Blob([e.data], { 'type' : 'audio/wav' })
-    const fd = new FormData();
-    fd.append('fname', num+'.wav');
-    fd.append('data', blob);
-    num++
-    const audio = document.querySelector('#ouraudio')
-    const audioURL = window.URL.createObjectURL(blob);
-    audio.src = audioURL;
-    //console.log(fd.get('data').size)
-    //console.log(blob.size)
-      $.ajax({
-      type: 'POST',
-      url: 'http://localhost:8000/voice-checker',
-      data: fd ,
-      processData: false,
-      contentType: false
-    }).done(function(data) {
-         console.log(data);
-    });
-    console.log('ondataavailable')
-  }*/
+  navigator.mediaDevices.getUserMedia(constraints).then(onSuccess).catch((err)=>{console.error(err)})
+} else {
+  console.error('getUserMedia not supported on your browser!')
 }
-
-navigator.mediaDevices.getUserMedia(constraints).then(onSuccess).catch((err)=>{console.error(err)})
