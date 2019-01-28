5f643ea2bff7913fe23d8532c16215be24219c2b
Akash <akashnainani9@gmail.com>
2019-01-28 19:57:08 +0530

    Api is predicting emotion
    model is integrated to predict sentiment.

--- server/server.py
+++ server/server.py
@@ -3,9 +3,14 @@ from flask_cors import CORS
 import pandas as pd
 import numpy as np
 from werkzeug.utils import secure_filename
-#import librosa
-#from keras.models import load_model
-
+import librosa
+import wave as wa
+import os
+import tensorflow as tf
+from keras.models import load_model
+global graph,model
+graph = tf.get_default_graph()
+files=[]
 app = Flask(__name__)
 CORS(app)
 
@@ -20,23 +25,39 @@ def prePro(x):
     res= np.expand_dims(res, axis=2)
     return res
 	
-#model = load_model('rnn_mfcc_model_ver_1.h5')
+model = load_model('rnn_mfcc_model_ver_1.h5')
 @app.route("/voice-checker",methods=["POST"])
 def secr():
     #print(request.data)
     a=request.files.get('data')
-    a.save(request.form.get('fname'))
-    #print(a)
-    return("server_response")
+    fname=request.form.get('fname')
+  #  files.append(fname)
+    a.save(fname)
+    x=toMfcc(fname)
+    res=prePro(x)
+  #  print(res)
+    with graph.as_default():
+      result=model.predict(res)
+    fresult=result.argmax(axis=1)
+   # print(a)
+    #x=wa.open(io.BytesIO(z),mode='rb')
+    print(fresult)
+    os.remove(fname)
+    val=fresult[0]
+    return("Emotion:"+str(val))
 '''
     #audio_file=request.files
-    x=toMfcc(a)
+    x=toMfcc(a)s
     res=prePro(x)
     print(res)
-    result=model.predict(res)
+    ressult=model.predict(res)
     result=result.argmax(axis=1)
     return(result)
 '''
-
+def deleteTempAudioFile(f):#use this when we can detect connection is closed
+    for file in f:
+	    os.remove(file)
+		
+		
 if __name__ == "__main__":
     app.run(port='8000')    
