e29d6387b8345fea5538e244a55c7abc9c3bfdb9
Akash <akashnainani9@gmail.com>
2019-02-04 22:23:59 +0530

    emotion normalization

--- server/server.py
+++ server/server.py
@@ -9,12 +9,18 @@ import os
 import tensorflow as tf
 from keras.models import load_model
 global graph,model,emotion
+
+
+#each time the server starts is equal to a new session on call
+
+
+
 graph = tf.get_default_graph()
 files=[]
 app = Flask(__name__)
 CORS(app)
 emotion=["neutral"," calm","happy","sad","angry","fearful","disgust","surprised"]
-
+count=0
 def toMfcc(file):
    X, sample_rate = librosa.load(file, res_type='kaiser_fast')
    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
@@ -30,6 +36,7 @@ model = load_model('rnn_mfcc_model_ver_1_CPU.h5')
 @app.route("/voice-checker",methods=["POST"])
 def secr():
     #print(request.data)
+    global count
     a=request.files.get('data')
     fname=request.form.get('fname')
   #  files.append(fname)
@@ -45,8 +52,10 @@ def secr():
    # print(fresult)
     os.remove(fname)
     val=fresult[0]
-    print(emotion[val])
-    return("Emotion:"+emotion[val])
+    em=emotion_norm(val)
+ #   print(emotion[val])
+    print(emotion[em])
+    return("Emotion:"+emotion[em])
 '''
     #audio_file=request.files
     x=toMfcc(a)s
@@ -59,7 +68,36 @@ def secr():
 def deleteTempAudioFile(f):#use this when we can detect connection is closed
     for file in f:
 	    os.remove(file)
-		
-		
+emo=0		
+def emotion_norm(v):
+    reduced_emo=emotion_red(v)
+    global emo
+    if reduced_emo==2:
+        emo+=6
+        if emo<-5:
+          emo+=3
+        if emo<-15:
+          emo+=10		
+    elif reduced_emo==0:
+        emo-=2
+    else:
+        emo-=4
+	   
+    print(emo)   
+    if emo>=10:
+       return 4
+    elif emo<0:
+       return 2
+    else:
+       return 0
+
+def emotion_red(v):
+    if v==4 or v==5 or v==6:
+       return 2
+    elif v==0 or v==1 or v==3:
+       return 0
+    else:
+       return 1
+
 if __name__ == "__main__":
     app.run(port='8000')    
