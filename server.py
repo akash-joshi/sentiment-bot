2b7a55d3ee18a4ab68f02b7f98f36bc3a85b7cdc
Unknown <jakash1997@gmail.com>
2019-02-17 21:43:13 +0530

    Gid

--- server.py
+++ server.py
@@ -11,18 +11,20 @@ from keras.models import load_model
 global graph,model,emotion
 
 app = Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-'''
-#each time the server starts is equal to a new session on call
 
-@app.route('/')
-def main():
-    return render_template('index.html')
-'''
 graph = tf.get_default_graph()
 files=[]
 
+def _build_cors_prelight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 emotion=["neutral"," calm","happy","sad","angry","fearful","disgust","surprised"]
 count=0
@@ -39,91 +41,32 @@ def prePro(x):
 	
 model = load_model('server/rnn_mfcc_model_ver_1_CPU.h5')
 
-def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
-                attach_to_all=True, automatic_options=True):
-    """Decorator function that allows crossdomain requests.
-      Courtesy of
-      https://blog.skyred.fi/articles/better-crossdomain-snippet-for-flask.html
-    """
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        """ Determines which methods are allowed
-        """
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        """The decorator function
-        """
-        def wrapped_function(*args, **kwargs):
-            """Caries out the actual cross domain code
-            """
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            h['Access-Control-Allow-Credentials'] = 'true'
-            h['Access-Control-Allow-Headers'] = \
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
-
 @app.route("/voice-checker",methods=["POST","OPTIONS"])
-@crossdomain(origin='*')
 def secr():
-    #print(request.data)
-    global count
-    a=request.files.get('data')
-    fname=request.form.get('fname')
-  #  files.append(fname)
-    a.save(fname)
-    x=toMfcc(fname)
-    res=prePro(x)
-  #  print(res)
-    with graph.as_default():
-      result=model.predict(res)
-    fresult=result.argmax(axis=1)
-   # print(a)
-    #x=wa.open(io.BytesIO(z),mode='rb')
-   # print(fresult)
-    os.remove(fname)
-    val=fresult[0]
-    em=emotion_norm(val)
- #   print(emotion[val])
-    print(emotion[em])
-    return("Emotion:"+emotion[em])
-'''
-    #audio_file=request.files
-    x=toMfcc(a)s
-    res=prePro(x)
-    print(res)
-    ressult=model.predict(res)
-    result=result.argmax(axis=1)
-    return(result)
-'''
+    if request.method == "OPTIONS": # CORS preflight
+      return _build_cors_prelight_response()
+    elif equest.method == "POST":
+      #print(request.data)
+      global count
+      a=request.files.get('data')
+      fname=request.form.get('fname')
+   #  files.append(fname)
+      a.save(fname)
+      x=toMfcc(fname)
+      res=prePro(x)
+   #  print(res)
+      with graph.as_default():
+         result=model.predict(res)
+      fresult=result.argmax(axis=1)
+      # print(a)
+      #x=wa.open(io.BytesIO(z),mode='rb')
+      # print(fresult)
+      os.remove(fname)
+      val=fresult[0]
+      em=emotion_norm(val)
+   #   print(emotion[val])
+      print(emotion[em])
+      return _corsify_actual_response("Emotion:"+emotion[em])
 
 
 def deleteTempAudioFile(f):#use this when we can detect connection is closed
