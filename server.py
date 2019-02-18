44b5b805d9fe3e01a790879eb000bb878cce88d1
Unknown <jakash1997@gmail.com>
2019-02-18 10:08:42 +0530

    Fuck

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import (Flask,request,render_template)
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 import pandas as pd
 import numpy as np
 from werkzeug.utils import secure_filename
@@ -11,20 +11,18 @@ from keras.models import load_model
 global graph,model,emotion
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+#each time the server starts is equal to a new session on call
+
+@app.route('/')
+def main():
+    return render_template('index.html')
 
 graph = tf.get_default_graph()
 files=[]
 
-def _build_cors_prelight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
-
-def _corsify_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
 
 emotion=["neutral"," calm","happy","sad","angry","fearful","disgust","surprised"]
 count=0
@@ -42,31 +40,38 @@ def prePro(x):
 model = load_model('server/rnn_mfcc_model_ver_1_CPU.h5')
 
 @app.route("/voice-checker",methods=["POST","OPTIONS"])
+@cross_origin()
 def secr():
-    if request.method == "OPTIONS": # CORS preflight
-      return _build_cors_prelight_response()
-    elif equest.method == "POST":
-      #print(request.data)
-      global count
-      a=request.files.get('data')
-      fname=request.form.get('fname')
-   #  files.append(fname)
-      a.save(fname)
-      x=toMfcc(fname)
-      res=prePro(x)
-   #  print(res)
-      with graph.as_default():
-         result=model.predict(res)
-      fresult=result.argmax(axis=1)
-      # print(a)
-      #x=wa.open(io.BytesIO(z),mode='rb')
-      # print(fresult)
-      os.remove(fname)
-      val=fresult[0]
-      em=emotion_norm(val)
-   #   print(emotion[val])
-      print(emotion[em])
-      return _corsify_actual_response("Emotion:"+emotion[em])
+    #print(request.data)
+    global count
+    a=request.files.get('data')
+    fname=request.form.get('fname')
+  #  files.append(fname)
+    a.save(fname)
+    x=toMfcc(fname)
+    res=prePro(x)
+  #  print(res)
+    with graph.as_default():
+      result=model.predict(res)
+    fresult=result.argmax(axis=1)
+   # print(a)
+    #x=wa.open(io.BytesIO(z),mode='rb')
+   # print(fresult)
+    os.remove(fname)
+    val=fresult[0]
+    em=emotion_norm(val)
+ #   print(emotion[val])
+    print(emotion[em])
+    return("Emotion:"+emotion[em])
+'''
+    #audio_file=request.files
+    x=toMfcc(a)s
+    res=prePro(x)
+    print(res)
+    ressult=model.predict(res)
+    result=result.argmax(axis=1)
+    return(result)
+'''
 
 
 def deleteTempAudioFile(f):#use this when we can detect connection is closed
