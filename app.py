a9ff2ec52c4d4a2f21d391124d9b27f0449ebc0b
Akash <akashnainani9@gmail.com>
2019-02-21 12:30:18 +0530

    front response

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
-    
+
+
+@app.route("/voice-checker",methods=["POST","OPTIONS"])
+def secr():
+    a=request.files.get('data')
+    fname=request.form.get('fname')
+    return (""+fname)
+
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
 
