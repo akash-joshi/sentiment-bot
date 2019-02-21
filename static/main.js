25b1a643bacd308931b684eb66fe7422c15c3a67
Unknown <jakash1997@gmail.com>
2019-02-21 13:59:16 +0530

    Adding feedback

--- static/main.js
+++ static/main.js
@@ -4,6 +4,16 @@ const running = document.querySelector('#running')
 const { MediaRecorder, Blob } = window
 const {startStream,stopStream} = AVStream
 
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5c6e51a13341d22d9ce55141/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
 let num=1;
 let flag=true
 // disable stop button while not recording
