983003d303eb85f5cf061b9373d274bb9dee2a99
Unknown <jakash1997@gmail.com>
2019-03-29 16:34:31 +0530

    Pushed

--- watson2.py
+++ watson2.py
@@ -1,9 +1,6 @@
 import watson_developer_cloud
 import json
 from flask import Flask, session, redirect, url_for, escape, request
-#from google.cloud import speech
-#from google.cloud.speech import enums
-#from google.cloud.speech import types
 
 
 version = '2018-11-08'
@@ -19,22 +16,6 @@ def init_watson(version, iam_apikey, url):
         url = url
         )
     return service
-
-'''def transcribe_gcs_long(file_path):
-    client = speech.SpeechClient.from_service_account_json('/path/to/Api/')
-
-    with io.open(file_path, 'rb') as audio_file:
-        content = audio_file.read()
-
-    audio = types.RecognitionAudio(content=content)
-    config = types.RecognitionConfig(sample_rate_hertz=8000,language_code='en-US')
-
-    operation = client.long_running_recognize(config, audio)
-    response = operation.result(timeout=90)
-
-    for result in response.results:
-        print(u'Transcript: {}'.format(result.alternatives[0].transcript))
-    decode_text(result.alternatives[0].transcript)'''
         
 def decode_text(text):
     result = [x.strip() for x in text.split('.')]
@@ -53,7 +34,6 @@ def index():
 
 @app.route('/create_session', methods = ['POST'])
 def create_session():
-    #service = session['service']
     service.set_default_headers({'x-watson-learning-opt-out': "true"})
     response = service.create_session(
         assistant_id = assistant_id
@@ -73,23 +53,22 @@ def delete_session():
 
     return 'ok'
 
-@app.route('/message', methods = ['GET', 'POST'])
+@app.route('/message', methods = ['POST'])
 def message():
-    if request.method == 'POST':
-        print(request.form['emotion'])
-        response = service.message(
-            assistant_id = assistant_id,
-            session_id = session['session_id'],
-            input={
-                'message_type': 'text',
-                'text': request.form['message'] + "," + request.form['emotion']
-                }
-            ).get_result()
-        
-        print(response)
-        
-        return '<form action="" method="post"><h1>' + response["output"]['generic'][0]['text'] + '</h1><input type = text name = "message"/><br><input type="radio" name="emotion" value="anger"> Anger<br><input type="radio" name="emotion" value="noanger"> NoAnger<br><input type = "submit" value = "Send"/></form>'
-    return '<form action="" method="post"><input type = text name = "message"/><br><input type="radio" name="emotion" value="anger"> Anger<br><input type="radio" name="emotion" value="noanger"> NoAnger<br><input type = "submit" value = "Send"/></form>'
+    got = request.get_json(force=True)
+    emotion = got['emotion']
+    message = got['message']
+    response = service.message(
+        assistant_id = assistant_id,
+        session_id = got['session_id'],
+        input={
+            'message_type': 'text',
+            'text': message + "," + emotion
+            }
+        ).get_result()
+    
+    print(response)
+    return 'lol'
 
 
 service = init_watson(version, iam_apikey, url)
